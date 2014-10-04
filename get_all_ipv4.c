2b5ce9657ceae7885ad00191a08222f8e9030495
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-04 21:23:27 +0800

    finish get_all_ipv4

--- get_all_ipv4.c
+++ get_all_ipv4.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 	}
 	
 	while (1){
-		printf ("==========================================");
+		printf ("==========================================\n");
 
 		n = recvfrom(sock, buffer, sizeof(buffer), 0, NULL, NULL);
 		printf ("%d bytes read\n", n);
