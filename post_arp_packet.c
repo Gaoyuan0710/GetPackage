b11700abc55fe803ffd22055213ff0c8bdcf4726
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-06 16:31:14 +0800

    add file post_arp_packet.c

---
+++ post_arp_packet.c
@@ -0,0 +1,59 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  post_arp_packet.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月06日 13时31分44秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/uf_ether.h>
+#include <net/if_arp.h>
+#include <netpacket/packet.h>
+#include <net/if.h>
+#include <net/ethernet.h>
+
+#define BUFLINE 42
+
+int main(int argc, char *argv[])
+{
+	int skfd, n;
+	char buf[BUFLEN] = {0};
+	struct ether_header *eth;
+	struct ether_arp *arp;
+	struct sockaddr_ll toaddr;
+	struct in_addr targetIP, srcIP;
+	struct ifreq ifr;
+
+	unsigned char src_mac[ETH_ALEN] = {0};
+	unsigned char dst_mac[ETH_ALEN] = {0xff, 0xff, 0xff, 0xff, 0xff,
+										0xff};
+
+	if (3 != argc){
+		
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
