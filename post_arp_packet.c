30cd2eeef82deb45e581eee04c010a1befc0a897
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-08 16:19:36 +0800

    finish post_arp_packet.c

--- post_arp_packet.c
+++ post_arp_packet.c
@@ -27,18 +27,20 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-#include <netinet/uf_ether.h>
+#include <netinet/if_ether.h>
 #include <net/if_arp.h>
 #include <netpacket/packet.h>
 #include <net/if.h>
 #include <net/ethernet.h>
+#include <arpa/inet.h>
 
 #define BUFLINE 42
 
 int main(int argc, char *argv[])
 {
 	int skfd, n;
-	char buf[BUFLEN] = {0};
+	int i;
+	char buf[BUFLINE] = {0};
 	struct ether_header *eth;
 	struct ether_arp *arp;
 	struct sockaddr_ll toaddr;
@@ -64,7 +66,7 @@ int main(int argc, char *argv[])
 	bzero(&toaddr, sizeof(toaddr));
 	bzero(&ifr, sizeof(ifr));
 
-	strcpy(if.ifr_name, argv[1]);
+	strcpy(ifr.ifr_name, argv[1]);
 
 	if (-1 == ioctl(skfd, SIOCGIFINDEX, &ifr)){
 		perror("get dev index error");
@@ -74,7 +76,7 @@ int main(int argc, char *argv[])
 	toaddr.sll_ifindex = ifr.ifr_ifindex;
 	printf ("Interface Index: %d\n", ifr.ifr_ifindex);
 
-	if (-1 == ioctl(skfd, SIOCGIFADDR, *ifr)){
+	if (-1 == ioctl(skfd, SIOCGIFADDR, &ifr)){
 		perror("Get IP addr error");
 		return -4;
 	}
@@ -82,6 +84,52 @@ int main(int argc, char *argv[])
 	srcIP.s_addr = ((struct sockaddr_in *)&(ifr.ifr_ifru.ifru_addr))->
 		sin_addr.s_addr;
 
+	printf ("IP addr: %s\n", inet_ntoa(((struct sockaddr_in *)&(ifr.ifr_ifru.ifru_addr)
+						)->sin_addr));
+
+
+	if (-1 == ioctl(skfd, SIOCGIFHWADDR, &ifr)){
+		perror("get dev MAC addr error:");
+
+		return -1;
+	}
+
+	memcpy(src_mac, ifr.ifr_ifru.ifru_hwaddr.sa_data, ETH_ALEN);
+	printf ("MAC:");
+
+	for (i = 0; i < 6; i++){
+		printf ("%02x", src_mac[i]);
+
+		if (i + 1 != 6){
+			printf("-");
+		}
+	}
+	printf ("\b\n");
+
+	eth = (struct ether_header *)buf;
+	memcpy(eth->ether_dhost, dst_mac, ETH_ALEN);
+	memcpy(eth->ether_shost, src_mac, ETH_ALEN);
+	eth->ether_type = htons(ETHERTYPE_ARP);
+
+	arp = (struct arphdr *)(buf + sizeof(struct ether_header));
+	arp->arp_hrd = htons(ARPHRD_ETHER);
+	arp->arp_pro = htons(ETHERTYPE_IP);
+
+	arp->arp_hln = ETH_ALEN;
+	arp->arp_pln = 4;
+
+	arp->arp_op = htons(ARPOP_REQUEST);
+
+	memcpy(arp->arp_sha, src_mac, ETH_ALEN);
+	memcpy(arp->arp_spa, &srcIP, 4);
+
+	inet_pton(AF_INET, argv[2], &targetIP);
+	memcpy(arp->arp_tpa, &targetIP, 4);
+
+	toaddr.sll_family = PF_PACKET;
+	n = sendto(skfd, buf, BUFLINE, 0, (struct sockaddr *) & toaddr, sizeof(toaddr));
+
+	close(skfd);
 
 
 	return EXIT_SUCCESS;
