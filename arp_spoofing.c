304b27a42264b7195fec68031f333fd82529d64f
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-13 21:13:59 +0800

    add file arp_spoofing.c .h

---
+++ arp_spoofing.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  arp_spoofing.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月13日 20时11分50秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+
+	return EXIT_SUCCESS;
+}
+
---
+++ arp_spoofing.h
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  arp_spoofing.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月13日 20时12分23秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+ *        Company:  Class 1204 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+#ifndef _ARP_SPOOFING_H_
+#define _ARP_SPOOFING_H_
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <net/ethernet.h>
+#include <net/ip.h>
+#include <netinet/tcp.h>
+#include
+#endif
+
